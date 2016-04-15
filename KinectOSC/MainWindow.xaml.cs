a80f9bc8995a0c135f7ce8897a3e247127ee1cc1
nryas <nryas@me.com>
2016-04-15 22:46:16 +0900

    init commit

---
+++ KinectOSC/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Kinect;
+
+namespace KinectOSC
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        KinectSensor kinect;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                kinect = KinectSensor.GetDefault();
+                if (kinect == null)
+                {
+                    throw new Exception("Kinectを開けません");
+                }
+                kinect.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Close();
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (kinect != null)
+            {
+                kinect.Close();
+                kinect = null;
+            }
+        }
+    }
+}
